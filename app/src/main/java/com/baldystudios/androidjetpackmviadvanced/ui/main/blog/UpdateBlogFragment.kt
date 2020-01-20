a4269cdbd4db96ac149d4b8d0e76d35bca7dc244
Shafayat <mindfreakicup33@gmail.com>
2020-01-20 18:59:15 +0600

    Updating a Blog Post

--- app/src/main/java/com/baldystudios/androidjetpackmviadvanced/ui/main/blog/UpdateBlogFragment.kt
+++ app/src/main/java/com/baldystudios/androidjetpackmviadvanced/ui/main/blog/UpdateBlogFragment.kt
@@ -1,16 +1,29 @@
 package com.baldystudios.androidjetpackmviadvanced.ui.main.blog
 
+import android.app.Activity
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.*
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.baldystudios.androidjetpackmviadvanced.R
+import com.baldystudios.androidjetpackmviadvanced.ui.*
 import com.baldystudios.androidjetpackmviadvanced.ui.main.blog.state.BlogStateEvent
+import com.baldystudios.androidjetpackmviadvanced.ui.main.blog.viewmodel.getUpdatedBlogUri
 import com.baldystudios.androidjetpackmviadvanced.ui.main.blog.viewmodel.onBlogPostUpdateSuccess
 import com.baldystudios.androidjetpackmviadvanced.ui.main.blog.viewmodel.setUpdatedBlogFields
+import com.baldystudios.androidjetpackmviadvanced.util.Constants
+import com.baldystudios.androidjetpackmviadvanced.util.ErrorHandling
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.fragment_update_blog.*
+import okhttp3.MediaType
 import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import java.io.File
 
 class UpdateBlogFragment : BaseBlogFragment() {
 
@@ -28,6 +41,21 @@ class UpdateBlogFragment : BaseBlogFragment() {
         setHasOptionsMenu(true)
 
         subscribeObservers()
+
+        image_container.setOnClickListener {
+            if (stateChangeListener.isStoragePermissionGranted()) {
+                pickFromGallery()
+            }
+        }
+    }
+
+    private fun pickFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*"
+        val mimeType = arrayOf("image/jpeg", "image/png", "image/jpg")
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeType)
+        intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+        startActivityForResult(intent, Constants.GALLERY_REQUEST_CODE)
     }
 
     private fun subscribeObservers() {
@@ -67,16 +95,96 @@ class UpdateBlogFragment : BaseBlogFragment() {
         blog_body.setText(updatedBlogBody)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+
+                Constants.GALLERY_REQUEST_CODE -> {
+                    data?.data?.let { uri ->
+                        activity?.let {
+                            launchImageCrop(uri)
+                        }
+                    } ?: showErrorDialog(ErrorHandling.ERROR_SOMETHING_WRONG_WITH_IMAGE)
+                }
+
+                CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
+                    Log.d(TAG, "CROP: CROP_IMAGE_ACTIVITY_REQUEST_CODE")
+                    val result = CropImage.getActivityResult(data)
+                    val resultUri = result.uri
+                    Log.d(TAG, "CROP: CROP_IMAGE_ACTIVITY_REQUEST_CODE: uri: $resultUri")
+                    viewModel.setUpdatedBlogFields(
+                        title = null,
+                        body = null,
+                        uri = resultUri
+                    )
+                }
+
+                CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE -> {
+                    showErrorDialog(ErrorHandling.ERROR_SOMETHING_WRONG_WITH_IMAGE)
+                }
+
+            }
+        }
+    }
+
+    private fun launchImageCrop(uri: Uri?) {
+
+        context?.let {
+            CropImage.activity(uri)
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .start(it, this)
+        }
+
+    }
+
     private fun saveChanges() {
         var multipartBody: MultipartBody.Part? = null
-        viewModel.setStateEvent(
-            BlogStateEvent.UpdatedBlogPostEvent(
-                blog_title.text.toString(),
-                blog_body.text.toString(),
-                multipartBody
+        viewModel.getUpdatedBlogUri()?.let { imageUri ->
+            imageUri.path?.let { filePath ->
+                val imageFile = File(filePath)
+                Log.d(TAG, "UpdateBlogFragment: imageFile: $imageFile")
+                val requestBody = RequestBody.create(
+                    MediaType.parse("image/*"),
+                    imageFile
+                )
+                multipartBody = MultipartBody.Part.createFormData(
+                    "image",
+                    imageFile.name,
+                    requestBody
+                )
+            }
+        }
+
+        multipartBody?.let {
+            viewModel.setStateEvent(
+                BlogStateEvent.UpdatedBlogPostEvent(
+                    blog_title.text.toString(),
+                    blog_body.text.toString(),
+                    it
+                )
+            )
+
+            stateChangeListener.hideSoftKeyboard()
+        } ?: showErrorDialog(ErrorHandling.ERROR_MUST_SELECT_IMAGE)
+    }
+
+    private fun showErrorDialog(errorMessage: String) {
+        stateChangeListener.onDataStateChange(
+            DataState(
+                Event(
+                    StateError(
+                        Response(
+                            errorMessage, ResponseType.Dialog()
+                        )
+                    )
+                ),
+                Loading(false),
+                Data(Event.dataEvent(null), null)
             )
         )
     }
+
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
@@ -90,6 +198,7 @@ class UpdateBlogFragment : BaseBlogFragment() {
                 saveChanges()
                 return true
             }
+
         }
 
         return super.onOptionsItemSelected(item)
