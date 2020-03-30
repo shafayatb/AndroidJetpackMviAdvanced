e66b9b7b06b1446a1464cbc95e26257eab7a31cd
Shafayat <mindfreakicup33@gmail.com>
2020-03-30 22:18:54 +0600

    Clear fields after creating blog.

--- app/src/main/java/com/baldystudios/androidjetpackmviadvanced/ui/main/create_blog/CreateBlogFragment.kt
+++ app/src/main/java/com/baldystudios/androidjetpackmviadvanced/ui/main/create_blog/CreateBlogFragment.kt
@@ -112,7 +112,7 @@ constructor(
         viewModel.stateMessage.observe(viewLifecycleOwner, Observer { stateMessage ->
 
             stateMessage?.let {
-                if (it.equals(SUCCESS_BLOG_CREATED)) {
+                if (it.response.message.equals(SUCCESS_BLOG_CREATED)) {
                     viewModel.clearNewBlogFields()
                 }
                 uiCommunicationListener.onResponseReceived(
@@ -127,7 +127,11 @@ constructor(
         })
     }
 
-    fun setBlogProperties(title: String?, body: String?, image: Uri?) {
+    fun setBlogProperties(
+        title: String? = "",
+        body: String? = "",
+        image: Uri?
+    ) {
         if (image != null) {
             requestManager
                 .load(image)
